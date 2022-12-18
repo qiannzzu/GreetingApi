28eeb46bc6e7afc517574ea396af898242a065e2
Reni Chu <Reni.Chu@advantech.com>
2022-12-18 19:24:30 +0800

    ver4 first commit

--- routes/routes.js
+++ routes/routes.js
@@ -1,39 +1,28 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
-const path = require('node:path');
-var fs = require('fs');
-
-function getAge(year,v) { // get user age
-    var age = year - v.DateOfBirth.getUTCFullYear()
-    return age;
-}
 
 //Get user in specific date birth
 router.get('/getUser', async (req, res) => {
     try {
-        const year = new Date().getUTCFullYear(); // testcase '2000-12-22'was 50years old of peter
-        const month = new Date().getMonth();
-        const day = new Date().getDate();
-        const data = await User.find() // all user data
+        const month = new Date().getMonth();//'1985-08-08'
+        const day = new Date().getDate();//'1985-08-08'
+        const data = await User.find()
         var birthDayUser = [];
+        const js = data[0].DateOfBirth
         data.map((v)=>{
             if(v.DateOfBirth.getMonth() === month && v.DateOfBirth.getDate() == day){ //if date,month same => birthday
-                var age = getAge(year,v)
-                if(age>49){
-                    birthDayUser.push(v.FirstName)
-                }
+                birthDayUser.push(v)
+                
             }
         })
-        var message = [];
+        var birthDayGreet = [];
         birthDayUser.map((v)=>{
-            message.push(`"title":"Subject: Happy birthday!",
-            "content":"Happy birthday, dear ${v}!",
-            "picurl":"https://storage.googleapis.com/image_carpool_bucket/birthdayGreeting.jpg"
-            `)
+            birthDayGreet.push(`Subject: Happy birthday! 
+            Happy birthday, dear ${v.LastName},${v.FirstName}!`)
+            
         })
-        
-        res.json(message)
+        res.json(birthDayGreet)
     }
     catch (error) {
         res.status(500).json({ message: error.message })
