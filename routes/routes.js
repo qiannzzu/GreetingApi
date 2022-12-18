451b6c09c72600814f98623cc34c38d6ec23611c
Reni Chu <Reni.Chu@advantech.com>
2022-12-18 11:28:25 +0800

    ver2 first commit

--- routes/routes.js
+++ routes/routes.js
@@ -2,17 +2,23 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 
-
-//Get all Method
-router.get('/getAllUser', async (req, res) => {
-    try {
-        const data = await User.find();
-        res.json(data)
+function getBirthState(userName,gender) {
+    if(gender === 'Male'){
+        return(`Subject: Happy birthday!
+        Happy birthday, dear ${userName}!
+        We offer special discount 20% off for the following items:
+        White Wine, iPhone X
+        `)
+    }else if(gender === 'Female'){
+        return(`Subject: Happy birthday!
+        Happy birthday, dear ${userName}!
+        We offer special discount 50% off for the following items:
+        Cosmetic, LV Handbags
+        `)
+    }else{ //gender cannot be determine
+        return 0;
     }
-    catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
+}
 
 //Get user in specific date birth
 router.get('/getUser', async (req, res) => {
@@ -21,20 +27,13 @@ router.get('/getUser', async (req, res) => {
         const day = new Date().getDate();//'1985-08-08'
         const data = await User.find()
         var birthDayUser = [];
-        const js = data[0].DateOfBirth
         data.map((v)=>{
             if(v.DateOfBirth.getMonth() === month && v.DateOfBirth.getDate() == day){ //if date,month same => birthday
-                birthDayUser.push(v.FirstName)
-                
+                var message = getBirthState( v.FirstName,v.Gender)
+                if(message != 0){birthDayUser.push(message)} //have message then push
             }
         })
-        var birthDayGreet = [];
-        birthDayUser.map((v)=>{
-            birthDayGreet.push(`Subject: Happy birthday! 
-            Happy birthday, dear ${v}!`)
-            
-        })
-        res.json(birthDayGreet)
+        res.json(birthDayUser)
     }
     catch (error) {
         res.status(500).json({ message: error.message })
